6eb263b2533d9bd1034b2cfab5fdd6bc710984c0
Taka <tkgitttr8@gmail.com>
2018-06-10 18:28:07 +0900

    make rule for result

--- app/controllers/p0kerhands_controller.rb
+++ app/controllers/p0kerhands_controller.rb
@@ -1,11 +1,33 @@
 class P0kerhandsController < ApplicationController
   def home
-    @hands = params[:hands]
+    @cards = params[:hands].split
+    @suits = @cards.map { |c| c[0] }
+    @nums  = @cards.map { |c| c[1].to_i }
+
+    if @suits.uniq.length == 1 && @nums.max - @nums.min == 4
+      @result = 'ストレートフラッシュ'
+    elsif @nums.count(@nums.max_by { |v| @nums.count(v) }) == 4
+      @result = 'フォー・オブ・ア・カインド'
+    elsif @nums.uniq.count == 2
+      @result = 'フルハウス'
+    elsif @suits.uniq.length == 1
+      @result = 'フラッシュ'
+    elsif @nums.max-@nums.min == 4
+      @result = 'ストレート'
+    elsif @nums.count(@nums.max_by { |v| @nums.count(v) }) == 3
+      @result = 'スリー・オブ・ア・カインド'
+    elsif @nums.uniq.length == 3
+      @result = 'ツーペア'
+    elsif @nums.uniq.length == 4
+      @result = 'ワンペア'
+    else
+      @result = 'ハイカード'
+    end
   end
 
-  def create
-    @hands = Hand.new(article_params)
-      render root_path
-  end
+  # def create
+  #     @cards = Hand.new(params[:hands])
+  #     render root_path
+  # end
 
 end
