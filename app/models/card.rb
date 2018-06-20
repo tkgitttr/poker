4cee6fc4955e1d5beb6bff9d3e020f0e720681ba
Taka <tkgitttr8@gmail.com>
2018-06-20 14:11:46 +0900

    不要なコメントアウトの削除

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,17 +1,7 @@
 class Card < ApplicationRecord
 
   VALID_CARD_REGEX = /[CDHS]([1-9]|1[0-3])\z/ #CDHSのいずれか+1~13までの数字
-  # 消す# validates :all_card, presence: true, length: {maximum: 19} #3文字*5+4スペース
-  # 消す# validates :first_card, presence: true, format: {with: VALID_CARD_REGEX}
-  # 消す# before_validation :card_exist #用法間違っている？
   validate :card_format
-  # validate :card_unique
-
-  # def card_exist
-  #   if all_card == nil || all_card.split(" ").length != 5
-  #     errors.add(" ",'5つのカード指定文字を半角スペース区切りで入力してください。（例："S1 H3 D9 C13 S11"）')
-  #   end
-  # end
 
   def card_format
     if all_card.split(" ").length == 5
@@ -32,13 +22,4 @@ class Card < ApplicationRecord
     end
   end
 
-  # def card_unique
-  #   if all_card.split(" ").uniq.length < 5
-  #     errors.add(" ", "カードが重複しています。")
-  #   end
-  # end
-
-  # if validate(:card_format) #もしエラーが一つでもあれば，このエラーメッセージを足したいがうまくいかない
-  #   @card.errors.add(" ", "半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。")
-  # end
 end
