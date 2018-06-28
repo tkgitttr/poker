26b5f5014e366f91c222d68a91f18ac816a2581b
Taka <tkgitttr8@gmail.com>
2018-06-28 18:43:59 +0900

    モデルのテストは，最小限は完成させた．ViewとControllerはこれから

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -12,34 +12,51 @@ RSpec.describe Card, type: :model do
     end
 
     context "正しいフォーマットで入力したとき" do
-      it ":all_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:all_card)
-      end
-      it ":first_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:first_card)
-      end
-      it ":second_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:second_card)
-      end
-      it ":third_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:third)
-      end
-      it ":fourth_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:fourth_card)
-      end
-      it ":fifth_cardがバリデーションに通る" do
-        expect(good_card).to be_valid(:fifth_card)
+      it ":バリデーションに通る" do
+        expect(good_card).to be_valid
       end
     end
 
     context "誤ったフォーマットで入力したとき" do
+
       it "バリデーションに通らない" do
-        expect(bad_card.errors_on(:first_card, context: :card_format).size).to eql 1
+        bad_card.valid?
+        expect(bad_card.errors.messages.size).to eq 1
       end
-      it "バリデーションに通らない(書き方の試行錯誤)" do
-        bad_card[:all_card].valid?
-        expect(bad_card.errors(:first_card).size).to eq 1
+
+      it "first_cardのみが不正でエラーになる" do
+        card = Card.new({all_card:"F50 S2 S3 S4 S5", first_card: "F50", second_card: "S2", third_card: "S3",
+                  fourth_card: "S4", fifth_card: "S5"})
+        expect(card).not_to be_valid
+        # 以下のテストは，全部通ってしまったりするので消す
+        # expect(card.errors[:first_card]).to be_present
+        # expect { warn  "1番目のカード指定文字が不正です。 (#{:first_card})" }.to output("1番目のカード指定文字が不正です。 (#{:first_card})\n").to_stderr
+      end
+
+      it "second_cardのみが不正でエラーになる" do
+        card = Card.new({all_card:"S1 A3 S3 S4 S5", first_card: "S1", second_card: "A3", third_card: "S3",
+                         fourth_card: "S4", fifth_card: "S5"})
+        expect(card).not_to be_valid
+      end
+
+      it "third_cardのみが不正でエラーになる" do
+        card = Card.new({all_card:"S1 S2 75 S4 S5", first_card: "S1", second_card: "S2", third_card: "75",
+                         fourth_card: "S4", fifth_card: "S5"})
+        expect(card).not_to be_valid
+      end
+
+      it "fourth_cardのみが不正でエラーになる" do
+        card = Card.new({all_card:"S1 S2 S3 S90 S5", first_card: "S1", second_card: "S2", third_card: "S3",
+                         fourth_card: "S90", fifth_card: "S5"})
+        expect(card).not_to be_valid
+      end
+
+      it "fifth_cardのみが不正でエラーになる" do
+        card = Card.new({all_card:"S1 S2 S3 S4", first_card: "S1", second_card: "S2", third_card: "S3",
+                         fourth_card: "S4", fifth_card: nil})
+        expect(card).not_to be_valid
       end
     end
 
+  #errrorメッセージなどはここでテストすべきか？
 end
