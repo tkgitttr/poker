d67a5b11e66bf48c37e962aa2c24274b668947ed
Taka <tkgitttr8@gmail.com>
2018-07-05 16:23:14 +0900

    ロジックを一つにまとめる以外は，最低限の機能を実装した

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 # before do
+#   #Factory.Botがうまく使えないため，ベタ打ちで
 #   @params = FactoryBot.attributes_for(:good_cards)
 # end
+
 RSpec.describe "Cards", type: :request do
 
   let(:request_header) do
@@ -11,6 +13,9 @@ RSpec.describe "Cards", type: :request do
 
   let(:good_cards) do
     '{"cards": ["H1 H13 H12 H11 H10", "H9 C9 S9 H2 C12","C13 D12 C11 H8 H7"]}'
+  end
+  let(:wrong_cards) do
+    '{"cards": ["H1 H13 H12 H11 H10", "H9 C9 S95 G2 C12","C13 D12 C11 H8 H7"]}'
   end
 
   describe "POST /api/v1/cards/check" do
@@ -33,9 +38,27 @@ RSpec.describe "Cards", type: :request do
         }.to change { Card.count }.by(3)
       end
 
-    context "間違ったパラメータを送ったとき" do
-      it "エラーメッセージが返ってくる"
-      it "Cardレコードが増えない"
+    context "間違ったパラメータを含むリクエストを送ったとき" do
+      it "エラーメッセージを含むJSONが返ってくる" do
+        post "/api/v1/cards/check", params: wrong_cards, headers:  request_header
+        # expect(response).to include("error")
+        body = JSON.parse(response.body)
+        # expect(body["result"]).to eq [{"card"=>"H1 H13 H12 H11 H10", "hand"=>"ストレートフラッシュ", "best"=>true},{"card"=>"C13 D12 C11 H8 H7", "hand"=>"ハイカード", "best"=>false}]
+        expect(body["result"][0]["card"]).to eq "H1 H13 H12 H11 H10"
+        expect(body["result"][0]["hand"]).to eq "ストレートフラッシュ"
+        expect(body["result"][0]["best"]).to eq true
+        expect(body["result"][1]["card"]).to eq "C13 D12 C11 H8 H7"
+        expect(body["result"][1]["hand"]).to eq "ハイカード"
+        expect(body["result"][1]["best"]).to eq false
+        expect(body["error"][0]["card"]).to eq "H9 C9 S95 G2 C12"
+        expect(body["error"][0]["msg"]).to eq ["  3番目のカード指定文字が不正です。 (S95)","  4番目のカード指定文字が不正です。 (G2)","  半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"]
+      end
+
+      it "正しいCardセットの数だけCardレコードが増える" do
+        expect {
+          post "/api/v1/cards/check", params: wrong_cards, headers:  request_header
+        }.to change { Card.count }.by(2) #2セット：正，1セット：誤　
+      end
     end
 
   end
