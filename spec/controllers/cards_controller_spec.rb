41b651ff2ecfe67b6d55ba0c782b81a91bf57e4f
Taka <tkgitttr8@gmail.com>
2018-07-27 13:51:40 +0900

    contorollerテストを書いた

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe CardsController, type: :controller do
 
   let(:invalid_session) do
     {all_card: "S1 S2 S3 S4 S56", first_card: "S1", second_card: "S2",
-     third_card: "S3", fourth_card: "S4", fifth_card: "S56", result: "ハイカード"}
+     third_card: "S3", fourth_card: "S4", fifth_card: "S56", result: nil}
   end
 
   describe "GET #index" do
@@ -36,21 +36,11 @@ RSpec.describe CardsController, type: :controller do
       it "cardにセッションが保存されている" do
         expect(controller.instance_variable_get("@card")).to eq session[:card]
       end
-      context "@card.saveが成功したとき" do
-        it "resultにsessionが保存されている" do
-          card = Card.new(valid_attributes)
-          expect(card.save).to be_truthy
-          get :index, params: {}, session: valid_session
-          expect(controller.instance_variable_get("@result")).to eq session[:result]
-        end
+      it "resultにsessionが保存されている" do
+        expect(controller.instance_variable_get("@result")).to eq session[:result]
       end
-      context "@card.saveが失敗したとき" do
-        it "resultにsessionが保存されていない" do
-          card = Card.new(invalid_attributes)
-          expect(card.save).to be_falsey
-          get :index, params: {}, session: invalid_session
-          expect(controller.instance_variable_get("@result")).not_to eq session[:result]
-        end
+      it "errorsにsessionが保存されている" do
+        expect(controller.instance_variable_get("@errors")).to eq session[:card_errors]
       end
     end
     context "セッションに誤った変数が保存されているとき" do
@@ -62,31 +52,55 @@ RSpec.describe CardsController, type: :controller do
         get :index, params: {}, session: invalid_session
         expect(controller.instance_variable_get("@result")).to eq nil
       end
+      it "errorsにsessionが保存されていない" do
+        get :index, params: {}, session: invalid_session
+        expect(controller.instance_variable_get("@errors")).to eq nil
+      end
     end
   end
 
   describe "POST #create" do
     context "valid params が送られてきたとき" do
-      it ":all_card にparams が代入される" do
+      before do
         post :create, params: {card: valid_attributes}
+      end
+      it ":all_card にparams が代入される" do
         expect(controller.instance_variable_get("@card")[:all_card]).to eq "S1 S2 S3 S4 S5"
+      end
+      it "service.saveが成功する" do
+        service = CardFormService.new(valid_attributes[:all_card])
+        expect(service.save).to eq true
+      end
+      it "session[:result]に役が保存されている" do
+        expect(session[:result]).to eq valid_session[:result]
+      end
+      it "session[:card_erorrs]にnilが保存されている" do
+        expect(session[:card_errors]).to eq nil
+      end
+      it "session[:all_card]にカードが保存されている" do
+        expect(session[:all_card]).to eq valid_session[:all_card]
       end
     end
     context "invalid paramが送られてきたとき" do
-      it ":all_card にparams が代入されない" do
+      before do
         post :create, params: {card: invalid_attributes}
+      end
+      it ":all_card にparams が代入されない" do
         expect(controller.instance_variable_get("@card")[:all_card]).not_to eq "S1 S2 S3 S4 S5"
       end
-
-    end
-    it "カードの役を正しく判定する" do
-      post :create, params: {card: valid_attributes}
-      expect(session[:result]).to eq "ストレートフラッシュ"
-    end
-    it "session にカードと役が保存されている" do
-      post :create, params: {card: valid_attributes}
-      expect(session).to include :all_card, :first_card, :second_card, :third_card,
-                                 :fourth_card, :fifth_card, :result
+      it "service.saveが失敗する" do
+        service = CardFormService.new(invalid_attributes[:all_card])
+        expect(service.save).to eq false
+      end
+      it "session[:result]に何も保存されていない" do
+        expect(session[:result]).to eq ""
+      end
+      it "session[:card_errors]にエラーメッセージが保存されている" do
+        expect(session[:card_errors]).not_to eq nil
+      end
+      it "session[:all_card]にカードが保存されている" do
+        expect(session[:all_card]).to eq invalid_session[:all_card]
+      end
     end
     it "indexにリダイレクトする" do
       post :create, params: {card: valid_attributes}, session: valid_session
